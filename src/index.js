4cb6c20d6627341f8f1a11a3366e07111e9d7909
face-usp <uspface@gmail.com>
2026-05-28 23:21:28 +0530

    refactor: utils to response success and rejection

--- src/index.js
+++ src/index.js
@@ -3,12 +3,18 @@
 import dotenv from "dotenv"
 import mongoose from "mongoose";
 import connectDB from "./db/index.js";
-
+import { app } from "./app.js";
 dotenv.config({
     path: './env'
 });
 
-connectDB();
+connectDB().then(()=>{
+    app.listen(process.env.PORT || 8000), ()=>{
+        console.log(`Connected on port: ${process.env.PORT}`)
+    }
+}).catch((error)=> {
+    console.log("Not connected to port",error)
+})
 
 // Traditional method or old way to connect DB 
 // import express from "express";
---
+++ src/utils/asynchandler.js
@@ -0,0 +1,20 @@
+export {asyncHandler}
+
+// Try catch way of writing function
+// const asyncHandler=(fn)=> async (req,res,next) => {
+//     try{
+//         await fn(req,res,next);
+//     } catch(error){
+//         res.status(error.code || 500).json({
+//             success : false,
+//             message: error.message
+//         })
+//     }
+// }
+
+// Promise method of writing it
+const asyncHandler = (requestHandler) => {
+(req,res,next) => {
+    Promise.resolve(requestHandler(req,res,next)).catch((err)=> next(err));
+}
+}
